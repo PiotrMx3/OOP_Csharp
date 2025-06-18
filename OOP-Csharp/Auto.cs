c10079e76d73de6e48a3a1930386d7f1a20f08f1
Piotr Mulinski <piotr.mulinski@icloud.com>
2025-06-18 21:38:15 +0200

    Attributen oefenigen klaar

---
+++ OOP-Csharp/Auto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Csharp
+{
+
+ enum MilieuNormen {Euro3, Euro4, Euro5, Euro6};
+
+internal class Auto
+    {
+        public int KilometerStand = 5;
+        public int Benzine = 10;
+        public DateTime LaatsteOnderhoud = DateTime.Now;
+        public static MilieuNormen Huidigenorm;
+
+
+        public static void PrintOnderhoudsrapport(Auto auto, int i)
+        {
+            TimeSpan service = DateTime.Today - auto.LaatsteOnderhoud;
+
+
+              Console.WriteLine($"Gegevens Auto nr {i + 1}:\n" +
+                   $"Kilometerstand: {auto.KilometerStand} km\n" +
+                   $"Benzine: {auto.Benzine} L\n" +
+                   $"Laatste onderhoud: {auto.LaatsteOnderhoud:d}\n" +
+                   $"Milieunorm: {Huidigenorm}\n");
+
+            if (service.Days > 180)
+            {
+                Console.WriteLine($"Auto nr {i + 1} heeft nood aan onderhoudsbeurt!");
+                Console.WriteLine($"Dagen sinds laatste onderhoud: {service.Days}");
+
+            }
+        }
+
+    }
+
+
+}
--- OOP-Csharp/Program.cs
+++ OOP-Csharp/Program.cs
@@ -6,55 +6,110 @@
 
         static void Main(string[] args)
         {
-            /*
-           ATTRIBUTEN
-
-
-            text from hier
-
-
-
-           */
-
-
-
-
-
-
-
 
             /*
-             DATE TIME 
 
-            DateTimeOefeningen.CurrentTime();
-            DateTimeOefeningen.CreateTime();
-            DateTimeOefeningen.AddTime();
-            DateTimeOefeningen.ReadTime();
-            DateTimeOefeningen.FormattedTime();
-            DateTimeOefeningen.ParsedTime();
-            DateTimeOefeningen.DiffTime();
+            ATTRIBUTEN
+
+            */
+
+            //Auto nieuweAuto1 = new Auto();
+            //Auto nieuweAuto2 = new Auto();
+
+            //nieuweAuto1.KilometerStand = 100;
+            //nieuweAuto1.Benzine = 50;
+            //nieuweAuto1.LaatsteOnderhoud = DateTime.Now;
+
+            //nieuweAuto2.LaatsteOnderhoud = new DateTime(2024, 1, 11, 20, 00, 00);
+
+            //Console.WriteLine($"Kilometerstand: {nieuweAuto1.KilometerStand}");
+            //Console.WriteLine($"Benzine: {nieuweAuto1.Benzine}");
+            //Console.WriteLine($"Laatste onderhoud: {nieuweAuto1.LaatsteOnderhoud}");
+
+            //Console.WriteLine();
+
+            //Console.WriteLine($"Kilometerstand: {nieuweAuto2.KilometerStand}");
+            //Console.WriteLine($"Benzine: {nieuweAuto2.Benzine}");
+            //Console.WriteLine($"Laatste onderhoud: {nieuweAuto2.LaatsteOnderhoud}");
 
 
 
-            Weekdagen dagKeuze;
-            Weekdagen andereKeuze;
+            Console.Write(
+                "Welcome in my Car genrator app Lets start !\n" 
+                + "How many car's you want to create ?: "
+                );
 
-            dagKeuze = Weekdagen.Donderdag;
+            int amount = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(dagKeuze);
+            Auto[] carsArray = new Auto[amount];
+            Auto.Huidigenorm = MilieuNormen.Euro6;
+
+            for (int i = 0; i < amount; i++)
+            {
+                Auto newCar = new Auto();
+
+                Console.Write($"Auto - {i + 1} Wat is de kilometer stand ?: ");
+                newCar.KilometerStand = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write($"Auto - {i + 1} Wat is de benzinde stand ?: ");
+                newCar.Benzine = Convert.ToInt32(Console.ReadLine());
 
 
-            Weekdagen keuze;
-            Console.WriteLine("Welke dag is het vandaag?");
-            Console.WriteLine($"{(int)Weekdagen.Maandag}. {Weekdagen.Maandag}");
-            Console.WriteLine($"{(int)Weekdagen.Dinsdag}. {Weekdagen.Dinsdag}");
+                Console.Write($"Auto - {i + 1} Wat is de datum van laatsteonderhoud (JJJ/M/D) ?: ");
+                newCar.LaatsteOnderhoud = DateTime.Parse(Console.ReadLine());
 
-            keuze = (Weekdagen)Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(keuze);
-             
-             
-          */
+                carsArray[i] = newCar;
+
+            }
+
+
+            for (int i = 0; i < carsArray.Length; i++)
+            {
+                Auto.PrintOnderhoudsrapport(carsArray[i], i);
+            }
+
+
+
+
+
+
+
+            /*
+
+            DATE TIME 
+
+            */
+
+
+
+            //DateTimeOefeningen.CurrentTime();
+            //DateTimeOefeningen.CreateTime();
+            //DateTimeOefeningen.AddTime();
+            //DateTimeOefeningen.ReadTime();
+            //DateTimeOefeningen.FormattedTime();
+            //DateTimeOefeningen.ParsedTime();
+            //DateTimeOefeningen.DiffTime();
+
+
+
+            //Weekdagen dagKeuze;
+            //Weekdagen andereKeuze;
+
+            //dagKeuze = Weekdagen.Donderdag;
+
+            //Console.WriteLine(dagKeuze);
+
+
+            //Weekdagen keuze;
+            //Console.WriteLine("Welke dag is het vandaag?");
+            //Console.WriteLine($"{(int)Weekdagen.Maandag}. {Weekdagen.Maandag}");
+            //Console.WriteLine($"{(int)Weekdagen.Dinsdag}. {Weekdagen.Dinsdag}");
+
+            //keuze = (Weekdagen)Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(keuze);
+
         }
     }
 }
